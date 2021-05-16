432fbb6c95cdf080d5500351f17767ca34463112
PoriyaAbdollahi <poriyaa58@gmail.com>
2021-05-16 10:41:47 +0430

    make entity connecting to Room by making the note class

---
+++ app/src/main/java/com/poriyaabdollahi/notenevis/Note.java
@@ -0,0 +1,43 @@
+package com.poriyaabdollahi.notenevis;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id ;
+
+    private String title ;
+
+    private String description;
+
+    private int priority;
+
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
